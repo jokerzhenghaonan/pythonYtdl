59cc159526f59cf42b3b9904f7a84058875ff9b8
jokerzhenghaonan <1289147676@qq.com>
2024-10-30 22:57:20 +0800

    vercel 2.0

--- CrawlData/crawl_data.py
+++ CrawlData/crawl_data.py
@@ -28,7 +28,12 @@ def list_formats(url):
 
 API_KEY = 'AIzaSyBeLbXYguI1KK7xZtc03SBSz9zuj5-FiIA'
 
-def get_audio_url(video_url):
+video_id = '_ZcmuKsyvzg'
+API_KEY = 'AIzaSyBeLbXYguI1KK7xZtc03SBSz9zuj5-FiIA'
+
+
+def get_audio_url1():
+    video_url = 'https://www.youtube.com/watch?v=_ZcmuKsyvzg&key=AIzaSyBeLbXYguI1KK7xZtc03SBSz9zuj5'  # 替换为实际的视频链接
     ydl_opts = {
         'format': 'bestaudio/best',  # 获取最佳音频格式
         'noplaylist': True,  # 如果链接是一个播放列表，仅下载第一个视频
@@ -42,9 +47,49 @@ def get_audio_url(video_url):
         print("Audio URL:", audio_url)
         return audio_url
 
+def get_video_info(video_id):
+    url = f'https://www.googleapis.com/youtube/v3/videos?id={video_id}&key={API_KEY}&part=status,snippet'
+    response = requests.get(url)
+    data = response.json()
+
+    if 'items' in data and len(data['items']) > 0:
+        return data['items'][0]  # 返回视频信息
+    else:
+        print("Error: Video not found or inaccessible.")
+        return None
+
+def get_audio_url(video_url):
+    video_id = video_url.split("v=")[-1]  # 提取视频ID
+    video_info = get_video_info(video_id)
+
+    if video_info and video_info['status']['privacyStatus'] == 'public':
+        ydl_opts = {
+            'format': 'bestaudio/best',  # 获取最佳音频格式
+            'noplaylist': True,  # 如果链接是一个播放列表，仅下载第一个视频
+            'quiet': True,  # 禁止额外输出
+            'extract_flat': True,  # 提取信息而不下载
+        }
+
+        try:
+            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(video_url, download=False)
+                audio_url = info_dict['url']
+                print("Audio URL:", audio_url)
+                return audio_url
+        except Exception as e:
+            print(f"Error occurred while extracting audio URL: {e}")
+            return None
+    else:
+        print("Error: Video is private or cannot be accessed.")
+        return None
+
 
 
 
 if __name__ == "__main__":
-    video_url = 'https://www.youtube.com/watch?v=_ZcmuKsyvzg&key=AIzaSyBeLbXYguI1KK7xZtc03SBSz9zuj5'  # 替换为实际的视频链接
-    get_audio_url(video_url)
+    video_url = "https://www.youtube.com/watch?v=_ZcmuKsyvzg"
+    audio_url = get_audio_url(video_url)
+    if audio_url:
+        print("Successfully retrieved audio URL.")
+    else:
+        print("Failed to retrieve audio URL.")
